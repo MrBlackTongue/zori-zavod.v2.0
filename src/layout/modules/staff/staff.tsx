8970ac30cea9791ef034f65fd0d8aca287456c69
Svetlana Tyshkevich <Lana-14-92@mail.ru>
2022-12-14 11:12:22 +0400

    add drawer

--- src/layout/modules/staff/staff.tsx
+++ src/layout/modules/staff/staff.tsx
@@ -10,162 +10,170 @@ import {
   Input,
   Form,
   Checkbox,
+  Drawer,
 } from 'antd';
 import {
   SyncOutlined,
   PlusOutlined,
   EditOutlined,
   DeleteOutlined,
+  CheckOutlined,
 } from '@ant-design/icons';
 import './staff.css';
 import type { ColumnsType, TableProps } from 'antd/es/table';
 
 const {Title} = Typography;
 
-interface DataType {
-  key: React.Key;
-  name: string;
-  surname: string;
-  number: number;
-  price: number;
-  staff: string;
-  // id: number;
-}
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    surname: 'John',
-    number: 6385649536,
-    price: 32,
-    staff: 'Да',
-    id: (
-      <Space>
-        <Tooltip title="Изменить" placement="bottomRight">
-          <Button
-            type="primary"
-            size="small"
-            shape="circle"
-            ghost
-            onClick={() => {
-              // this.getBlocksInfo(id);
-              // this.setState({
-              //   editModal: true,
-              // });
-            }}>
-            <EditOutlined/>
-          </Button>
-        </Tooltip>
-        <Tooltip title="Удалить" placement="bottomRight">
-          <Popconfirm
-            title="Вы действительно хотите удалить этого сотрудника?"
-            onConfirm={() => {
-              // this.deleteBlockInfo(id);
-            }}
-            okText="Да"
-            cancelText="Отмена">
-            <Button type="primary" size="small" shape="circle" ghost onClick={() => {
-            }}>
-              <DeleteOutlined/>
-            </Button>
-          </Popconfirm>
-        </Tooltip>
-      </Space>
-    ),
-  },
-  {
-    key: '2',
-    name: 'John',
-    surname: 'Mike',
-    number: 347547530,
-    price: 42,
-    staff: 'Да',
-    id: (
-      <Space>
-        <Tooltip title="Изменить" placement="bottomRight">
-          <Button
-            type="primary"
-            size="small"
-            shape="circle"
-            ghost
-            onClick={() => {
-              // this.getBlocksInfo(id);
-              // this.setState({
-              //   editModal: true,
-              // });
-            }}>
-            <EditOutlined/>
-          </Button>
-        </Tooltip>
-        <Tooltip title="Удалить" placement="bottomRight">
-          <Popconfirm
-            title="Вы действительно хотите удалить этого сотрудника?"
-            onConfirm={() => {
-              // this.deleteBlockInfo(id);
-            }}
-            okText="Да"
-            cancelText="Отмена">
-            <Button type="primary" size="small" shape="circle" ghost onClick={() => {
-            }}>
-              <DeleteOutlined/>
-            </Button>
-          </Popconfirm>
-        </Tooltip>
-      </Space>
-    ),
-  },
-];
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Имя',
-    dataIndex: 'name',
-    key: 'name',
-    defaultSortOrder: 'ascend',
-    sorter: (a, b) => a.name < b.name ? -1 : 1,
-  },
-  {
-    title: 'Фамилия',
-    dataIndex: 'surname',
-    key: 'surname',
-    sorter: (a, b) => a.surname < b.surname ? -1 : 1,
-
-  },
-  {
-    title: 'Телефон',
-    dataIndex: 'number',
-    key: 'number',
-  },
-  {
-    title: 'Ставка',
-    dataIndex: 'price',
-    key: 'price',
-    sorter: (a, b) => a.price - b.price,
-
-  },
-  {
-    title: 'Нанят',
-    dataIndex: 'staff',
-    key: 'staff',
-    sorter: (a, b) => a.staff < b.staff ? -1 : 1,
-
-  },
-  {
-    title: 'Действия',
-    dataIndex: 'id',
-    key: 'id',
-    width: 100,
-  },
-];
-
-const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
-  console.log('params', pagination, filters, sorter, extra);
-};
 
 const Staff = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  interface DataType {
+    key: React.Key;
+    name: string;
+    surname: string;
+    number: number;
+    price: number;
+    staff: string;
+    // id: number;
+  }
+
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      surname: 'John',
+      number: 6385649536,
+      price: 32,
+      staff: 'Да',
+      id: (
+        <Space>
+          <Tooltip title="Изменить" placement="bottomRight">
+            <Button
+              type="primary"
+              size="small"
+              shape="circle"
+              ghost
+              onClick={() => {
+                showDrawer()
+                // this.getBlocksInfo(id);
+                // this.setState({
+                //   editModal: true,
+                // });
+              }}>
+              <EditOutlined/>
+            </Button>
+          </Tooltip>
+          <Tooltip title="Удалить" placement="bottomRight">
+            <Popconfirm
+              title="Вы действительно хотите удалить этого сотрудника?"
+              onConfirm={() => {
+                // this.deleteBlockInfo(id);
+              }}
+              okText="Да"
+              cancelText="Отмена">
+              <Button type="primary" size="small" shape="circle" ghost onClick={() => {
+              }}>
+                <DeleteOutlined/>
+              </Button>
+            </Popconfirm>
+          </Tooltip>
+        </Space>
+      ),
+    },
+    {
+      key: '2',
+      name: 'John',
+      surname: 'Mike',
+      number: 347547530,
+      price: 42,
+      staff: 'Да',
+      id: (
+        <Space>
+          <Tooltip title="Изменить" placement="bottomRight">
+            <Button
+              type="primary"
+              size="small"
+              shape="circle"
+              ghost
+              onClick={() => {
+                showDrawer()
+                // this.getBlocksInfo(id);
+                // this.setState({
+                //   editModal: true,
+                // });
+              }}>
+              <EditOutlined/>
+            </Button>
+          </Tooltip>
+          <Tooltip title="Удалить" placement="bottomRight">
+            <Popconfirm
+              title="Вы действительно хотите удалить этого сотрудника?"
+              onConfirm={() => {
+                // this.deleteBlockInfo(id);
+              }}
+              okText="Да"
+              cancelText="Отмена">
+              <Button type="primary" size="small" shape="circle" ghost onClick={() => {
+              }}>
+                <DeleteOutlined/>
+              </Button>
+            </Popconfirm>
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Имя',
+      dataIndex: 'name',
+      key: 'name',
+      defaultSortOrder: 'ascend',
+      sorter: (a, b) => a.name < b.name ? -1 : 1,
+    },
+    {
+      title: 'Фамилия',
+      dataIndex: 'surname',
+      key: 'surname',
+      sorter: (a, b) => a.surname < b.surname ? -1 : 1,
+
+    },
+    {
+      title: 'Телефон',
+      dataIndex: 'number',
+      key: 'number',
+    },
+    {
+      title: 'Ставка',
+      dataIndex: 'price',
+      key: 'price',
+      sorter: (a, b) => a.price - b.price,
+
+    },
+    {
+      title: 'Нанят',
+      dataIndex: 'staff',
+      key: 'staff',
+      sorter: (a, b) => a.staff < b.staff ? -1 : 1,
+
+    },
+    {
+      title: 'Действия',
+      dataIndex: 'id',
+      key: 'id',
+      width: 100,
+    },
+  ];
+
+  const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+    console.log('params', pagination, filters, sorter, extra);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOOpen] = useState(false);
+
+  // Modal
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -176,6 +184,15 @@ const Staff = () => {
 
   const handleCancel = () => {
     setIsModalOpen(false);
+  };
+
+  // Drawer
+  const showDrawer = () => {
+    setIsDrawerOOpen(true);
+  };
+
+  const onCloseDrawer = () => {
+    setIsDrawerOOpen(false);
   };
 
   const onFinish = (values: string) => {
@@ -218,9 +235,9 @@ const Staff = () => {
         cancelText={'Отмена'}
         footer={[
           <Button form='add-new-worker' type="primary" htmlType="submit">
-            Добавить
+            <CheckOutlined />Добавить
           </Button>,
-          <Button form='add-new-worker' type="dashed" onClick={handleCancel}>
+          <Button form='add-new-worker' onClick={handleCancel}>
             Отмена
           </Button>
         ]}
@@ -270,14 +287,70 @@ const Staff = () => {
             wrapperCol={{offset: 8, span: 16}}>
             <Checkbox>Нанят</Checkbox>
           </Form.Item>
-
-          {/*<Form.Item wrapperCol={{ offset: 8, span: 16 }}>*/}
-          {/*  <Button type="primary" htmlType="submit">*/}
-          {/*    Submit*/}
-          {/*  </Button>*/}
-          {/*</Form.Item>*/}
         </Form>
       </Modal>
+      <Drawer
+        title="Редактирование сотрудника"
+        width={600}
+        onClose={onCloseDrawer}
+        open={isDrawerOpen}
+        bodyStyle={{ paddingBottom: 80 }}
+        extra={
+          <Space>
+            <Button onClick={onCloseDrawer}>Отмена</Button>
+            <Button onClick={onCloseDrawer} type="primary" form='change-worker' htmlType="submit">
+              Изменить
+            </Button>
+          </Space>
+        }
+      >
+        <Form
+          id='change-worker'
+          name="change-worker"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          labelCol={{span: 6}}
+          wrapperCol={{span: 16}}
+          style={{marginTop: 30}}
+        >
+          <Form.Item
+            label="Имя"
+            name="name"
+            rules={[{required: true, message: 'Пожалуйста введите имя'}]}
+          >
+            <Input/>
+          </Form.Item>
+          <Form.Item
+            label="Фамилия"
+            name="surname"
+            rules={[{required: true, message: 'Пожалуйста введите фамилию'}]}
+          >
+            <Input/>
+          </Form.Item>
+          <Form.Item
+            label="Телефон"
+            name="phoneNumber"
+          >
+            <Input/>
+          </Form.Item>
+          <Form.Item
+            label="Ставка"
+            name="salary"
+          >
+            <Input/>
+          </Form.Item>
+          <Form.Item
+            name="hired"
+            valuePropName="hired"
+            wrapperCol={{offset: 8, span: 16}}>
+            <Checkbox>Нанят</Checkbox>
+          </Form.Item>
+        </Form>
+      </Drawer>
     </div>
   );
 };
