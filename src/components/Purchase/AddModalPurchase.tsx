7b747293801da196e679da21de34b4a993dbe7eb
MrBlackTongue <Lana-14-92@mail.ru>
2023-03-09 13:50:12 +0400

    change AddModalPurchase

--- src/components/Purchase/AddModalPurchase.tsx
+++ src/components/Purchase/AddModalPurchase.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {AddItemProps, PurchaseType, ProductTypes} from "../../types";
-import {Form, Input, Modal, Select, InputNumber, DatePicker, Checkbox} from "antd";
+import {Form, Modal, Select, InputNumber, DatePicker, Checkbox} from "antd";
 import {getAllProducts} from "../../services";
 import {CheckboxChangeEvent} from "antd/es/checkbox";
 
@@ -8,125 +8,123 @@ const {Option} = Select;
 const dateFormatUser = 'DD.MM.YYYY';
 
 export const AddModalPurchase: React.FC<AddItemProps<PurchaseType>> = ({
-                                                                           isOpen,
-                                                                           addItem,
-                                                                           onCancel,
+                                                                         isOpen,
+                                                                         addItem,
+                                                                         onCancel,
                                                                        }) => {
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
-    const [products, setProducts] = useState<ProductTypes[]>();
-    const [selectedProduct, setSelectedProduct] = useState<ProductTypes>();
+  const [products, setProducts] = useState<ProductTypes[]>();
+  const [selectedProduct, setSelectedProduct] = useState<ProductTypes>();
 
-    const onChangeCheckbox = (e: CheckboxChangeEvent) => {
-        form.setFieldsValue({hired: e.target.checked});
-    }
+  const onChangeCheckbox = (e: CheckboxChangeEvent) => {
+    form.setFieldsValue({paid: e.target.checked});
+  }
 
-    const onChangeProduct = (values: string, option: any): ProductTypes => {
-        const product: ProductTypes = {
-            id: option.id,
-            title: values,
-        };
-        form.setFieldsValue({
-            product: product
-        });
-        setSelectedProduct(product)
-        return product
+  const onChangeProduct = (values: string, option: any): ProductTypes => {
+    const product: ProductTypes = {
+      id: option.id,
+      title: values,
     };
+    form.setFieldsValue({
+      product: product.id
+    });
+    setSelectedProduct(product)
+    return product
+  };
 
-    useEffect(() => {
-        getAllProducts().then((products) => {
-            setProducts(products);
-        });
-    }, []);
+  useEffect(() => {
+    getAllProducts().then((products) => {
+      setProducts(products);
+    });
+  }, []);
 
-    const handleOk = () => {
-        form
-            .validateFields()
-            .then((values) => {
-                form.resetFields();
-                setSelectedProduct(undefined)
-                addItem(values);
-            })
-            .catch((error) => {
-                console.log("Validate Failed:", error);
-            });
-    };
+  const handleOk = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        form.resetFields();
+        setSelectedProduct(undefined)
+        addItem(values);
+      })
+      .catch((error) => {
+        console.log("Validate Failed:", error);
+      });
+  };
 
-    return (
-        <Modal
-            title={`Добавление новой закупки`}
-            open={isOpen}
-            onCancel={() => {
-                onCancel()
-                setSelectedProduct(undefined)
-            }}
-            width={500}
-            okText={"Сохранить"}
-            cancelText={"Отмена"}
-            onOk={handleOk}
+  return (
+    <Modal
+      title={`Добавление новой закупки`}
+      open={isOpen}
+      onCancel={() => {
+        onCancel()
+        setSelectedProduct(undefined)
+      }}
+      width={500}
+      okText={"Сохранить"}
+      cancelText={"Отмена"}
+      onOk={handleOk}
+    >
+      <Form
+        form={form}
+        name="add-new-purchase"
+        initialValues={{
+          modifier: "public",
+        }}
+        labelCol={{span: 6}}
+        wrapperCol={{span: 16}}
+        style={{marginTop: 30}}
+      >
+        <Form.Item
+          label="Товар"
+          name="product"
+          rules={[{required: true, message: 'Пожалуйста выберите товар'}]}
         >
-            <Form
-                form={form}
-                name="add-new-purchase"
-                initialValues={{
-                    modifier: "public",
-                }}
-                labelCol={{span: 6}}
-                wrapperCol={{span: 16}}
-                style={{marginTop: 30}}
+          <div>
+            <Select
+              value={selectedProduct ? selectedProduct.title : undefined}
+              onChange={onChangeProduct}
             >
-                <Form.Item
-                    label="Товар"
-                    name="product"
-                    rules={[{required: true, message: "Выберите товар"}]}
-                >
-                    <div>
-                        <Select
-                            value={selectedProduct ? selectedProduct.title : undefined}
-                            onChange={onChangeProduct}
-                        >
-                            {products && products.length > 0 ?
-                                products.map(product => (
-                                    <Option id={product.id} key={product.id} value={product.title}>
-                                        {product.title}
-                                    </Option>
-                                )) : null}
-                        </Select>
-                    </div>
-                </Form.Item>
-                <Form.Item
-                    label="Цена"
-                    name="cost"
-                    rules={[{required: true, message: "Пожалуйста введите цену"}]}
-
-                >
-                    <InputNumber/>
-                </Form.Item>
-                <Form.Item
-                    label="Количество"
-                    name="amount"
-                    rules={[{required: true, message: "Пожалуйста введите количество"}]}
-                >
-                    <InputNumber/>
-                </Form.Item>
-                <Form.Item
-                    label="Дата"
-                    name="product"
-                    rules={[{type: 'object' as const, required: true, message: 'Пожалуйста введите дату'}]}
-                >
-                    <DatePicker
-                        style={{width: '100%'}}
-                        format={dateFormatUser}
-                    />
-                </Form.Item>
-                <Form.Item
-//                     label="Оплачено"
-                    name="paid"
-                    wrapperCol={{offset: 8, span: 16}}
-                >
-                    <Checkbox onChange={onChangeCheckbox}>Оплачено</Checkbox>
-                </Form.Item>
-            </Form>
-        </Modal>
-    );
+              {products && products.length > 0 ?
+                products.map(product => (
+                  <Option id={product.id} key={product.id} value={product.title}>
+                    {product.title}
+                  </Option>
+                )) : null}
+            </Select>
+          </div>
+        </Form.Item>
+        <Form.Item
+          label="Цена"
+          name="cost"
+          rules={[{required: true, message: "Пожалуйста введите цену"}]}
+        >
+          <InputNumber style={{width: "100%"}}/>
+        </Form.Item>
+        <Form.Item
+          label="Количество"
+          name="amount"
+          rules={[{required: true, message: "Пожалуйста введите количество"}]}
+        >
+          <InputNumber style={{width: "100%"}}/>
+        </Form.Item>
+        <Form.Item
+          label="Дата"
+          name="date"
+          rules={[{type: 'object' as const, required: true, message: 'Пожалуйста введите дату'}]}
+        >
+          <DatePicker
+            style={{width: '100%'}}
+            format={dateFormatUser}
+          />
+        </Form.Item>
+        <Form.Item
+          name="paid"
+          wrapperCol={{offset: 8, span: 16}}
+        >
+          <Checkbox onChange={onChangeCheckbox}>Оплачено</Checkbox>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 };
