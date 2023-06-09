2482a8693b9bd8ec3d2e110cdecc94b3fc94193d
MrBlackTongue <Lana-14-92@mail.ru>
2023-06-09 12:24:20 +0400

    fix authorization again

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
 } from '@ant-design/icons';
-import {Layout, Spin, theme} from 'antd';
+import {Layout, theme} from 'antd';
 import {MenuMain} from "./components/MenuMain/MenuMain";
 import {AppRoutes} from "./components/AppRoutes/AppRoutes";
-import {PageLoginForm} from "./pages/PageLoginForm/PageLoginForm";
 import {AuthContext} from "./components/Context/AuthContext";
+import {PageLoginForm} from "./pages/PageLoginForm/PageLoginForm";
 
 const {Header, Sider, Content} = Layout;
 
@@ -18,8 +18,8 @@ function App() {
   const location = useLocation();
   const {token: {colorBgContainer}} = theme.useToken();
 
-  const {isAuthenticated, logIn, logOut} = useContext(AuthContext);
-  const [isLoading, setIsLoading] = useState(true);
+  // Получите значение isAuthenticated из контекста AuthContext
+  const {isAuthenticated} = useContext(AuthContext);
 
   useEffect(() => {
     if (location.pathname === '/operation-accounting') {
@@ -29,61 +29,33 @@ function App() {
     }
   }, [location]);
 
-  useEffect(() => {
-    fetch(`http://localhost:8080/api/isAuthenticated`, {
-      method: 'GET',
-      credentials: 'include', // включает отправку cookies
-    })
-      .then(response => {
-        if (response.ok) {
-          logIn();
-        } else {
-          logOut();
-        }
-        setIsLoading(false);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        logOut();
-        setIsLoading(false);
-      });
-  }, [logIn, logOut]);
-
-  if (isLoading) {
-    return <Spin/>;  // можно заменить на кастомный компонент загрузки
-  }
-
-  if (!isAuthenticated) {
-    return (
-      <div className="login-form-container">
-        <PageLoginForm onLogin={logIn}/>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed} width={240} theme="light">
-          {/*<div className="logo" />*/}
-          <Header style={{padding: 0, background: colorBgContainer}}>
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: () => setCollapsed(!collapsed),
-            })}
-          </Header>
-          <MenuMain/>
-        </Sider>
-        <Layout className="site-layout" style={{minHeight: '100vh', height: '100vh', overflow: 'auto'}}>
-          <Header style={{padding: 0, background: colorBgContainer}}>
-            {/*<Title level={3}></Title>*/}
-          </Header>
-          <Content className='context-style'>
-            <AppRoutes/>
-          </Content>
+    isAuthenticated ? (
+      <div>
+        <Layout>
+          <Sider trigger={null} collapsible collapsed={collapsed} width={240} theme="light">
+            {/*<div className="logo" />*/}
+            <Header style={{padding: 0, background: colorBgContainer}}>
+              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: () => setCollapsed(!collapsed),
+              })}
+            </Header>
+            <MenuMain/>
+          </Sider>
+          <Layout className="site-layout" style={{minHeight: '100vh', height: '100vh', overflow: 'auto'}}>
+            <Header style={{padding: 0, background: colorBgContainer}}>
+              {/*<Title level={3}></Title>*/}
+            </Header>
+            <Content className='context-style'>
+              <AppRoutes/>
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
-    </div>
+      </div>
+    ) : (
+      <PageLoginForm/>
+    )
   );
 }
 
--- src/pages/PageLoginForm/PageLoginForm.tsx
+++ src/pages/PageLoginForm/PageLoginForm.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {LockOutlined, UserOutlined, EyeTwoTone, EyeInvisibleOutlined} from '@ant-design/icons';
 import {Button, Form, Input, Typography} from 'antd';
 import {loginUser} from "../../services";
+import {AuthContext} from "../../components/Context/AuthContext";
 
-export interface LoginFormProps {
-  onLogin: (newToken: string) => void;
-}
+export const PageLoginForm: React.FC = () => {
 
-export const PageLoginForm: React.FC<LoginFormProps> = ({
-                                                          onLogin,
-                                                        }) => {
   const {Title} = Typography;
   const [form] = Form.useForm();
+
+  const {logIn} = useContext(AuthContext);
 
   const onFinish = () => {
     form
@@ -19,7 +17,7 @@ export const PageLoginForm: React.FC<LoginFormProps> = ({
       .then((values) => {
         loginUser(values).then(response => {
           if (response && response.jwt) {
-            onLogin('Authenticated');
+            logIn();
           }
         })
       })
@@ -29,50 +27,43 @@ export const PageLoginForm: React.FC<LoginFormProps> = ({
   };
 
   return (
-    <Form
-      form={form}
-      className="login-form"
-      // initialValues={{remember: true}}
-      onFinish={onFinish}
-    >
-      <Form.Item>
-        <Title>Здравствуйте!</Title>
-      </Form.Item>
-      <Form.Item
-        name="username"
-        rules={[{required: true, message: 'введите ваш логин'}]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Логин"/>
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{required: true, message: 'введите ваш пароль'}]}
-      >
-        <Input.Password
-          prefix={<LockOutlined className="site-form-item-icon"/>}
-          type="password"
-          placeholder="Пароль"
-          visibilityToggle
-          iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
-        />
-      </Form.Item>
-      {/*<Form.Item>*/}
-      {/*<Form.Item name="remember" valuePropName="checked" noStyle>*/}
-      {/*  <Checkbox>Запомнить</Checkbox>*/}
-      {/*</Form.Item>*/}
-      {/*</Form.Item>*/}
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Войти
-        </Button>
-        {/*<a href="">Регистрация</a>*/}
-      </Form.Item>
-      <Form.Item>
-        {/*<a className="login-form-forgot" href="">*/}
-        {/*  Забыли пароль*/}
-        {/*</a>*/}
-      </Form.Item>
-    </Form>
+    <div className="login-form-container">
+      <Form
+        form={form}
+        className="login-form"
+        onFinish={onFinish}
+      >
+        <Form.Item>
+          <Title>Здравствуйте!</Title>
+        </Form.Item>
+
+        <Form.Item
+          name="username"
+          rules={[{required: true, message: 'введите ваш логин'}]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Логин"/>
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{required: true, message: 'введите ваш пароль'}]}
+        >
+          <Input.Password
+            prefix={<LockOutlined className="site-form-item-icon"/>}
+            type="password"
+            placeholder="Пароль"
+            visibilityToggle
+            iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
