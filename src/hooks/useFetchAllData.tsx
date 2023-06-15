21e76ed694a3c80cd93cd46e6bd459a945fbc305
MrBlackTongue <Lana-14-92@mail.ru>
2023-06-15 16:37:00 +0400

    change hooks

--- src/hooks/useFetchAllData.tsx
+++ src/hooks/useFetchAllData.tsx
@@ -63,105 +63,105 @@ export const useFetchAllData = (
 
   useEffect(() => {
     if (deps.depsStock) {
-      getAllStock().then((data) => {
-        setAllStock(data);
-      });
+      getAllStock()
+        .then((data) => setAllStock(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsStock]);
 
   useEffect(() => {
     if (deps.depsPurchase) {
-      getAllPurchase().then((data) => {
-        setAllPurchase(data);
-      });
+      getAllPurchase()
+        .then((data) => setAllPurchase(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsPurchase]);
 
   useEffect(() => {
     if (deps.depsProductBatch) {
-      getAllProductBatch().then((data) => {
-        setAllProductBatch(data);
-      });
+      getAllProductBatch()
+        .then((data) => setAllProductBatch(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsProductBatch]);
 
   useEffect(() => {
     if (deps.depsUnit) {
-      getAllUnit().then((data) => {
-        setAllUnit(data);
-      });
+      getAllUnit()
+        .then((data) => setAllUnit(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsUnit]);
 
   useEffect(() => {
     if (deps.depsOperation) {
-      getAllOperation().then((data) => {
-        setAllOperation(data);
-      });
+      getAllOperation()
+        .then((data) => setAllOperation(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsOperation]);
 
   useEffect(() => {
     if (deps.depsProductionType) {
-      getAllProductionType().then((data) => {
-        setAllProductionType(data);
-      });
+      getAllProductionType()
+        .then((data) => setAllProductionType(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsProductionType]);
 
   useEffect(() => {
     if (deps.depsOutput) {
-      getAllOutput().then((data) => {
-        setAllOutput(data);
-      });
+      getAllOutput()
+        .then((data) => setAllOutput(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsOutput]);
 
   useEffect(() => {
     if (deps.depsProduct) {
-      getAllProduct().then((data) => {
-        setAllProduct(data);
-      });
+      getAllProduct()
+        .then((data) => setAllProduct(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsProduct]);
 
   useEffect(() => {
     if (deps.depsProductGroup) {
-      getAllProductGroup().then((data) => {
-        setAllProductGroup(data);
-      });
+      getAllProductGroup()
+        .then((data) => setAllProductGroup(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsProductGroup]);
 
   useEffect(() => {
     if (deps.depsClient) {
-      getAllClient().then((data) => {
-        setAllClient(data);
-      });
+      getAllClient()
+        .then((data) => setAllClient(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsClient]);
 
   useEffect(() => {
     if (deps.depsEmployee) {
-      getAllEmployee().then((data) => {
-        setAllEmployee(data);
-      });
+      getAllEmployee()
+        .then((data) => setAllEmployee(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsEmployee]);
 
   useEffect(() => {
     if (deps.depsMeterType) {
-      getAllMeterType().then((data) => {
-        setAllMeterType(data);
-      });
+      getAllMeterType()
+        .then((data) => setAllMeterType(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsMeterType]);
 
   useEffect(() => {
     if (deps.depsMeter) {
-      getAllMeter().then((data) => {
-        setAllMeter(data);
-      });
+      getAllMeter()
+        .then((data) => setAllMeter(data))
+        .catch((error) => console.error("Ошибка при получении данных: ", error));
     }
   }, [deps.depsMeter]);
 
