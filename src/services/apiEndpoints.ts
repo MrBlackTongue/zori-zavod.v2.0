c390243d5ec40f9b939e39a40cc85590619085c9
MrBlackTongue <Lana-14-92@mail.ru>
2023-06-16 16:29:33 +0400

    change API_URL = 'http://zolotenkov.ru/api'

--- src/services/apiEndpoints.ts
+++ src/services/apiEndpoints.ts
@@ -1,5 +1,5 @@
 // export const API_URL = 'https://91.105.199.36/api'
-export const API_URL = 'https://zolotenkov.ru/api'
+export const API_URL = 'http://zolotenkov.ru/api'
 // export const API_URL = 'http://localhost:8090/api'
 
 export const EMPLOYEE = '/employee'
