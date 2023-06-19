a7bff72cf091ab614a23bb68b799c9054199903e
MrBlackTongue <Lana-14-92@mail.ru>
2023-06-19 12:39:13 +0400

    enabled interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,8 +7,8 @@ export const api = axios.create({
 });
 
 // Перенаправляет на страницу авторизации не авторизованного пользователя
-// api.interceptors.response.use((response) => response, (error) => {
-//   if (error.response?.status === 401) {
-//     window.location.assign('/login');
-//   }
-// });
+api.interceptors.response.use((response) => response, (error) => {
+  if (error.response?.status === 401) {
+    window.location.assign('/login');
+  }
+});
