8efd30a364e116dbd254bcb6ba4760d43941f1fa
Константин <glass.ocean10@gmail.com>
2023-07-04 16:18:40 +0300

    добавлены изображения, добавлены css стили, изменена разметка

--- src/pages/PageHome/PageHome.tsx
+++ src/pages/PageHome/PageHome.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Typography, Button, Space} from 'antd';
+import { Typography, Button, Space } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import '../../App.css';
+
 const { Title } = Typography;
 
 export const PageHome = () => {
@@ -19,17 +20,26 @@ export const PageHome = () => {
       top: '0',
       left: '0',
       display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: 'space-between',
+      alignItems: 'left',
+      padding: '30px',
       zIndex: 9999,
       backgroundColor: '#ffffff'
-    }}>
-      <div style={{ textAlign: 'center' }}>
-        <Title level={1}>Здарова заебал</Title>
-        <p>Купи наше приложение для ровных пацанов</p>
+    }} className={'container'}>
+      <div style={{ marginLeft: '100px' }} className={'container'}>
+        <img src="/logo.png" alt="Logo" style={{ height: '170px', marginTop: '-63px', marginLeft: '20px' }} />
+      </div>
+      <div className={'container'}>
+        <Title level={1} className={'landing-form'}>Здарова заебал</Title>
+        <p className={'landing-form'}>Купи наше приложение для ровных пацанов и отведай этих тёплых, мягких французских булок </p>
         <Space>
-          <Button type="primary" onClick={handleStartWork}>начать работу</Button>
+          <Button type="primary" onClick={handleStartWork} className={'landing-form-button'}>начать работу</Button>
         </Space>
+      </div>
+      <div>
+        <img src="/Analyse.png" alt="Analyse"
+             style={{ height: '300px', marginTop: '70px', marginRight: '60px' }}
+        />
       </div>
     </div>
   );
