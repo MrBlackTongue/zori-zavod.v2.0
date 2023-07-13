74822be854367a5aa6bef3c92ef5f4b4a899d17d
Константин <glass.ocean10@gmail.com>
2023-07-13 18:31:30 +0300

    добавлены новые изображения, изменены карточки, добавлен новый контейнер с описанием преимуществ

--- src/pages/PageHome/PageHome.tsx
+++ src/pages/PageHome/PageHome.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Typography, Button, Space, Card} from 'antd';
+import {Typography, Button, Space} from 'antd';
 import {useNavigate} from 'react-router-dom';
 import '../PageHome/PageHome.css';
 
 const {Title} = Typography;
-const {Meta} = Card;
+
+
 
 export const PageHome = () => {
   const navigate = useNavigate();
@@ -20,12 +21,8 @@ export const PageHome = () => {
         <img src="/logo.png" alt="Logo" className='logo-style'/>
         {/*</div>*/}
         <Space>
-          {/*<div className='button-registration'>*/}
-          <Button type="primary">Зарегистрироваться</Button>
-          {/*</div>*/}
-          {/*<div className='button-login'>*/}
           <Button type="dashed" onClick={handleStartWork}>Войти</Button>
-          {/*</div>*/}
+          <Button type="primary">Зарегистрироваться</Button>
         </Space>
       </div>
       <div className='container-block'>
@@ -46,28 +43,50 @@ export const PageHome = () => {
       <div className='containerBlock2'>
         <div className='containerText2'>
           <Title className='title-style-2'>Кому выгодно?</Title>
-          {/*<p className='text-style-2'>производствам, всяким бизнесам и в целом ровным пацанам и пацанессам</p>*/}
         </div>
         <div className='container-card'>
-            <Card
-              className='card-style'
-              cover={<img alt="manufacture" src="/manufacture1.png"/>}
-            >
-              <Meta title="Производствам"/>
-            </Card>
-            <Card
-              className='card-style'
-              cover={<img alt="manufacture" src="/manufacture1.png"/>}
-            >
-              <Meta title="Производствам"/>
-            </Card>
-            <Card
-              className='card-style'
-              cover={<img alt="manufacture" src="/manufacture1.png"/>}
-            >
-              <Meta title="Производствам"/>
-            </Card>
+            <div className='card-style'>
+              <img alt="manufacture" src="/manufacture1.png" className='card-image'/>
+              <p className='card-title'>производствам</p>
+            </div>
+          <div className='card-style'>
+            <img alt="manufacture" src="/manufacture1.png" className='card-image'/>
+            <p className='card-title'>производствам</p>
           </div>
+          <div className='card-style'>
+            <img alt="manufacture" src="/manufacture1.png" className='card-image'/>
+            <p className='card-title'>производствам</p>
+          </div>
+          <div className='card-style'>
+            <img alt="manufacture" src="/manufacture1.png" className='card-image'/>
+            <p className='card-title'>производствам</p>
+          </div>
+          </div>
+      </div>
+      <div className='containerBlock3'>
+        <div className='container-jumbotron'>
+          <img src="/info.png" alt="info" className='img-main'/>
+        </div>
+        <div className='container-text'>
+          <Title className='title-style-1'>Управление задачами</Title>
+          <p className='text-style-1'>
+            Используйте листы задач, расписание команды и календарь.
+            Добавляйте файлы, комментарии и делитесь своими разработками с командой.
+          </p>
+        </div>
+      </div>
+      <div className='containerBlock3'>
+        <div className='container-text'>
+          <Title className='title-style-1'>Управление временем</Title>
+          <p className='text-style-1'>
+            Следите за задачами, над которыми вы ежедневно работаете и анализируйте,
+            сколько времени вы потратили на каждый проект.
+            Вы можете использовать автоматический таймер или управлять им вручную.
+          </p>
+        </div>
+        <div className='container-jumbotron'>
+          <img src="/info2.png" alt="info2" className='img-main'/>
+        </div>
       </div>
     </div>
   );
