831e9d788faafe18bf4311a7b7e9f212d5267f6b
MrBlackTongue <Lana-14-92@mail.ru>
2023-07-13 11:05:14 +0400

    Change route "/" to PageLoginForm

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {PageHome} from "../../pages/PageHome/PageHome";
+// import {PageHome} from "../../pages/PageHome/PageHome";
 import {PageLoginForm} from "../../pages/PageLoginForm/PageLoginForm";
 import App from "../../App";
 
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<PageHome/>}/>
+      {/*<Route path="/" element={<PageHome/>}/>*/}
+      <Route path="/" element={<PageLoginForm/>}/>
       <Route path="/login" element={<PageLoginForm/>}/>
       <Route path="/*" element={<App/>}/>
     </Routes>
