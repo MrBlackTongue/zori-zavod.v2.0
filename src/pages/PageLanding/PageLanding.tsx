a35d661498facd5f732b5317e3d3aea7a6815c9b
Константин <glass.ocean10@gmail.com>
2023-07-19 15:16:22 +0300

    копирайт заменён на 2022 год, изменён размер кнопок "начать работу", в футере изменена кнопка "связаться с нами", заменён номер телефона для связи, удалены лишние стили, изменены отступы в конце блока с карточками

--- src/pages/PageLanding/PageLanding.tsx
+++ src/pages/PageLanding/PageLanding.tsx
@@ -33,10 +33,10 @@ export const PageLanding = () => {
             </Button>
           </Space>
         </div>
-        <img src="/Analyse.png" alt="Analyse" className='img-main container-jumbotron'/>
+        <img src="/Analyse.png" alt="Analyse" className='container-jumbotron'/>
       </div>
-      <div className='containerBlock2'>
-        <Title className='title-style-2 containerText2'>Кому выгодно?</Title>
+      <div className='container-Block-2'>
+        <Title className='title-style-2 container-Text-2'>Кому выгодно?</Title>
         <div className='container-card'>
           <div className='card-style'>
             <img alt="conveyor" src="/conveyor.png" className='card-image'/>
@@ -56,8 +56,8 @@ export const PageLanding = () => {
           </div>
         </div>
       </div>
-      <div className='containerBlock3'>
-        <img src="/info.png" alt="info" className='img-main container-jumbotron'/>
+      <div className='container-Block-3'>
+        <img src="/info.png" alt="info" className='container-jumbotron'/>
         <div className='container-text'>
           <Title className='title-style-1'>Управление задачами</Title>
           <p className='text-style-1'>
@@ -66,7 +66,7 @@ export const PageLanding = () => {
           </p>
         </div>
       </div>
-      <div className='containerBlock3'>
+      <div className='container-Block-3'>
         <div className='container-text'>
           <Title className='title-style-1'>Управление временем</Title>
           <p className='text-style-1'>
@@ -75,10 +75,10 @@ export const PageLanding = () => {
             Вы можете использовать автоматический таймер или управлять им вручную.
           </p>
         </div>
-        <img src="/info2.png" alt="info2" className='img-main container-jumbotron'/>
+        <img src="/info2.png" alt="info2" className='container-jumbotron'/>
       </div>
-      <div className='containerBlock3'>
-        <img src="/info3.png" alt="info3" className='img-main container-jumbotron'/>
+      <div className='container-Block-3'>
+        <img src="/info3.png" alt="info3" className='container-jumbotron'/>
         <div className='container-text'>
           <Title className='title-style-1'>Коммуникация команды</Title>
           <p className='text-style-1'>
@@ -87,8 +87,8 @@ export const PageLanding = () => {
           </p>
         </div>
       </div>
-      <div className='containerBlock4'>
-        <Title level={2} className='containerText2'>Мы поможем вам работать более эффективно</Title>
+      <div className='container-Block-4'>
+        <Title level={2} className='container-Text-2'>Мы поможем вам работать более эффективно</Title>
         <p className='text-style-3'>
           Мы предлагаем вашей команде полный сет инструментов для создания лучших проектных решений
         </p>
@@ -149,21 +149,22 @@ export const PageLanding = () => {
         </div>
       </div>
       <div className='container-block5'>
-        <img alt="Retail-Business" src="/Retail-Business.png" className="container-image-free1"/>
+        <img alt="Retail-Business" src="/Retail-Business.png" className="container-image-free"/>
         <div className='container-block-column'>
-          <Title level={1} className='containerText2'>Попробуйте Zolotenkov учёт прямо сейчас!</Title>
+          <Title level={1} className='container-Text-2'>Попробуйте Zolotenkov учёт прямо сейчас!</Title>
           <Space>
             <Button type="primary" className='start-button' onClick={handleStartWork}>Бесплатная версия</Button>
           </Space>
         </div>
-        <img alt="empresarial" src="/empresarial.png" className="container-image-free2"/>
+        <img alt="empresarial" src="/empresarial.png" className="container-image-free"/>
       </div>
       <div className='container-footer'>
-        <p className='text-footer'>Телефон: +7 (916) 051-63-23</p>
+        <p className='text-footer'>Телефон: +7 (968) 614-15-72</p>
+        <p className='text-footer2'>Связаться с нами:</p>
         <a href="https://t.me/AlAlon369" rel="noopener noreferrer">
-          <Button type="dashed" className='footer-button'>Связаться с нами</Button>
+            <img alt="icon-telegram" src="/icon-telegram.png" className="telegram-logo"/>
         </a>
-        <p className='text-footer2'>© Zolotenkov 2023</p>
+        <p className='text-footer3'>© Zolotenkov 2022</p>
       </div>
     </div>
   );
