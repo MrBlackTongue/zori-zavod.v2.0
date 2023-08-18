40f244b670b11ff5b6cbfec89342b32944e11688
MrBlackTongue <Lana-14-92@mail.ru>
2023-08-18 09:59:48 +0300

    Main: Mini fix

--- src/pages/PageLoginForm/PageLoginForm.tsx
+++ src/pages/PageLoginForm/PageLoginForm.tsx
@@ -21,6 +21,7 @@ export const PageLoginForm: React.FC = () => {
     form
       .validateFields()
       .then((values) => {
+        console.log('values', values)
         loginUser(values)
           .then(response => {
             if (response?.jwt) {
@@ -62,7 +63,7 @@ export const PageLoginForm: React.FC = () => {
           <Input
             size="large"
             prefix={<UserOutlined className="input-prefix-icon"/>}
-            placeholder="Почта"
+            placeholder="Логин"
           />
         </Form.Item>
 
