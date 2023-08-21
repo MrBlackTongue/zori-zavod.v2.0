5bee51d8ff00d8f9c37fb8d643b3e410aaf50058
MrBlackTongue <Lana-14-92@mail.ru>
2023-08-21 17:44:57 +0300

    Main: Delete console from Login

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {PageLanding} from "../../pages/PageLanding/PageLanding";
-import {PageLoginForm} from "../../pages/PageLoginForm/PageLoginForm";
+import {PageLogin} from "../../pages/PageLogin/PageLogin";
 import App from "../../App";
 
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<PageLanding/>}/>
-      <Route path="/login" element={<PageLoginForm/>}/>
+      <Route path="/login" element={<PageLogin/>}/>
       <Route path="/*" element={<App/>}/>
     </Routes>
   );
