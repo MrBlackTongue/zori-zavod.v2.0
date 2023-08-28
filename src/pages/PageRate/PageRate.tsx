ae1cb388b72aed7a87ce194c72810dd5d28da5f7
Константин <glass.ocean10@gmail.com>
2023-08-28 18:48:30 +0300

    DEV 1-121: изменён футер, добавлены новые стили, страничка оптимизирована под планшетную версию(768px)

--- src/pages/PageRate/PageRate.tsx
+++ src/pages/PageRate/PageRate.tsx
@@ -91,7 +91,7 @@ export const PageRate = () => {
         </Space>
       </div>
       <div className='rate-block flex row center-row'>
-        <div className='rate-text-block flex  column  center-column'>
+        <div className='rate-text-block flex  column'>
           <div className='rate-title'>Пробный период на 14 дней</div>
           <div className='rate-text'>
             Мы уверены в эффективности нашего приложения и хотим, чтобы вы лично убедились в этом.
@@ -99,31 +99,36 @@ export const PageRate = () => {
             className='text-bold'>бесплатный доступ к полному функционалу на 14 дней. </span>
             После завершения пробного периода вы сможете
             продолжить пользоваться нашим приложением за <span className='text-bold'>2990 рублей в месяц. </span>
-
           </div>
           <div className='button-block flex column'>
-            <Button type="primary" className='button-buy'>14 дней бесплатно</Button>
+            <Button type="primary" className='button-buy' onClick={() => setIsModalOpen(true)}>
+              14 дней бесплатно
+            </Button>
             <div className='mini-text center-text'>Далее 2990 ₽ в месяц</div>
           </div>
         </div>
         <img src="/images/rate_image2.png" alt="rate"
-             className='rate-jumbotron flex column center-row center-column'/>
+             className='rate-jumbotron flex column'/>
       </div>
-      <div className='rate-footer flex row center-row center-column'>
-        <div className='footer-group flex row center-row'>
-          <div className='rate-footer-block2 column'>
-            <p className='rate-footer-text-one'>Лазарь Олег Михайлович</p>
-            <p className='rate-footer-text-two'>Телефон: +7 (968) 614-15-72</p>
+      <div className='rate-footer flex column center-column'>
+        <div className='footer-group flex row center-row '>
+          <div className='rate-footer-block2 flex column '>
+            <p className='rate-footer-text-two'>Лазарь Олег Михайлович</p>
             <p className='rate-footer-text-two'>ИНН: 7710140679</p>
-            <p className='rate-footer-text-two'>Email: aloneofnight@mail.ru</p>
           </div>
-          <div className='footer-block flex column center-column'>
+          <div className='rate-footer-block flex column'>
+            <p className='rate-footer-text-two'>Email: support@zolotenkov.ru</p>
+            <p className='rate-footer-text-two'>Телефон: +7 (968) 614-15-72</p>
+          </div>
+          <div className='rate-footer-text-three flex column center-column'>
             <p className='rate-footer-text-two'>Связаться с нами:</p>
             <a href="https://t.me/AlAlon369" target='_blank' rel="noopener noreferrer">
               <img alt="icon-telegram" src="/images/footer_icon_telegram.png" className="rate-footer-telegram"/>
             </a>
-            <p className='rate-footer-text-three'>© Zolotenkov 2022-2023</p>
           </div>
+        </div>
+        <div className='rate-footer-block flex column'>
+        <p className='rate-footer-text-three'>© Zolotenkov 2022-2023</p>
         </div>
       </div>
       <CreateModalRegistrationUser
