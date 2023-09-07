739e5016cba86c465353225b54361682481d9392
Константин <glass.ocean10@gmail.com>
2023-09-07 13:01:51 +0300

    DEV-121:  Добавлено нижнее подчёркивание кнопки "забыли пароль?", при наведении на кнопку подчёркивание изчезает

--- src/pages/PageLanding/components/CreateModalRegistrationUser.tsx
+++ src/pages/PageLanding/components/CreateModalRegistrationUser.tsx
@@ -113,7 +113,7 @@ export const CreateModalRegistrationUser: React.FC<CreateModalProps<TypeUserProf
             Зарегистрироваться
           </Button>
           <div className='registration-login'>
-          <a href="/login" >У меня уже есть аккаунт</a>
+          <a href="/login" className='registration-login-text'>У меня уже есть аккаунт</a>
           </div>
           <div className='registration-text-offer'>
             Регистрируясь в сервисе Zolotenkov, вы принимаете условия
