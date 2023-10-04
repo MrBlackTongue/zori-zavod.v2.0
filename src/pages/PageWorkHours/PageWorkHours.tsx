f165f53b5bdb06b744117ad68f934ef0a207aa8d
Костя <glass.ocean10@gmail.com>
2023-10-04 18:38:25 +0300

    DEV-113: Добавлен datePicker для выбора рабочей недели

--- src/pages/PageWorkHours/PageWorkHours.tsx
+++ src/pages/PageWorkHours/PageWorkHours.tsx
@@ -1,82 +1,108 @@
 import React, {useState} from 'react';
-import {Typography, Space, Button, FloatButton,} from 'antd';
-import {SyncOutlined, PlusOutlined,} from '@ant-design/icons';
+import {Typography, Space, Button, FloatButton, DatePicker,} from 'antd';
+import {SyncOutlined, PlusOutlined, LeftOutlined, RightOutlined, } from '@ant-design/icons';
+import 'dayjs/locale/ru'
+import weekOfYear from "dayjs/plugin/weekOfYear";
 import '../../App.css'
 import {deleteUnitById} from "../../services";
-// import {TypeWorkHours, TypeUnitFormValue, TypeEmployeeFormValue, } from "../../types";
-// import {CreateModalUnit} from "../PageUnit/components/CreateModalUnit";
-// import {UpdateDrawerUnit} from "../PageUnit/components/UpdateDrawerUnit";
 import {TableWorkHours} from "./components/TableWorkHours";
+import dayjs from "dayjs";
 
 export const PageWorkHours: React.FC = () => {
 
   const {Title} = Typography;
+  dayjs.locale('ru');
+  dayjs.extend(weekOfYear);
 
   // Обновление таблицы, Открыть закрыть модальное окно, дравер
   const [isUpdateTable, setIsUpdateTable] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  // const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
-  // Обновить сотрудника
-  // const handleUpdateWorkHour = (values: TypeEmployeeFormValue): void => {
-  //   const employee: TypeEmployee = {
-  //     id: selectedEmployeeId,
-  //     firstName: values.firstName,
-  //     lastName: values.lastName,
-  //     phone: values.phone,
-  //     salaryRate: values.salaryRate,
-  //     hired: values.hired,
-  //   };
-  //   setIsDrawerOpen(false)
-  //   void updateEmployee(employee)
-  //   setIsUpdateTable(prevState => !prevState)
-  // };
+    // Установка текущей недели по умолчанию
+    const [selectedDate, setSelectedDate] = useState(dayjs().startOf('week'));
 
-  // Удалить запись из таблицы
-  const handleDeleteUnit = (id: number): void => {
-    void deleteUnitById(id)
-    setIsUpdateTable(prevState => !prevState)
+    // Переключение на предыдущую неделю
+    const prevWeek = () => {
+      setSelectedDate(prevDate => prevDate.subtract(1, 'week'));
+    };
+
+    // Переключение на следующую неделю
+    const nextWeek = () => {
+      setSelectedDate(prevDate => prevDate.add(1, 'week'));
+    };
+
+    // Обработчик изменения даты
+    const handleDateChange = (date: any) => {
+      setSelectedDate(dayjs(date));
+    };
+
+  const getWeekFormat = (date: dayjs.Dayjs | null) => {
+    if (!date || !date.isValid()) return "";
+
+    const startOfWeek = date.startOf('week');
+    const endOfWeek = date.endOf('week');
+    const weekNumber = startOfWeek.week();
+    return `неделя ${weekNumber}: ${startOfWeek.format('D MMM')} - ${endOfWeek.format('D MMM YYYY')}`;
   };
 
-  return (
-    <div style={{display: 'grid'}}>
-      <div className='centerTitle'>
-        <Title level={3}>Табель учёта рабочего времени</Title>
-        <Space>
-          <Button
-            type="dashed"
-            icon={<SyncOutlined/>}
-            onClick={() => setIsUpdateTable(prevState => !prevState)}
-            className='greenButton'
-          >
-            Обновить
-          </Button>
-          <Button
-            type="primary"
-            icon={<PlusOutlined/>}
-            onClick={() => setIsModalOpen(true)}
-          >
-            Добавить
-          </Button>
-        </Space>
-      </div>
-      <FloatButton.BackTop/>
-      <TableWorkHours
-        isUpdateTable={isUpdateTable}
-        // openDrawer={openDrawer}
-        onDelete={handleDeleteUnit}
-      />
-      {/*<CreateModalUnit*/}
-      {/*  isOpen={isModalOpen}*/}
-      {/*  createItem={handleCreateUnit}*/}
-      {/*  onCancel={() => setIsModalOpen(false)}*/}
-      {/*/>*/}
-      {/*<UpdateDrawerUnit*/}
-      {/*  isOpen={isDrawerOpen}*/}
-      {/*  selectedItemId={selectedUnitId}*/}
-      {/*  updateItem={handleUpdateUnit}*/}
-      {/*  onCancel={() => setIsDrawerOpen(false)}*/}
-      {/*/>*/}
-    </div>
-  );
-};
+  // Удалить запись из таблицы
+  //   const handleDeleteUnit = (id: number): void => {
+  //     void deleteUnitById(id)
+  //     setIsUpdateTable(prevState => !prevState)
+  //   };
+
+    return (
+        <div style={{display: 'grid'}}>
+          <div className='centerTitle'>
+            <Title level={3}>Табель учёта рабочего времени</Title>
+            <Space>
+              <div>
+                <LeftOutlined onClick={prevWeek} />
+                <DatePicker
+                    allowClear={false}
+                    picker="week"
+                    onChange={handleDateChange}
+                    value={selectedDate}
+                    format={getWeekFormat(selectedDate)}
+                    style={{width: '280px'}}
+                    className="no-clear-button"
+                />
+                <RightOutlined onClick={nextWeek} />
+              </div>
+              <Button
+                  type="dashed"
+                  icon={<SyncOutlined/>}
+                  onClick={() => setIsUpdateTable(prevState => !prevState)}
+                  className='greenButton'
+              >
+                Обновить
+              </Button>
+              <Button
+                  type="primary"
+                  icon={<PlusOutlined/>}
+                  onClick={() => setIsModalOpen(true)}
+              >
+                Добавить
+              </Button>
+            </Space>
+          </div>
+          <FloatButton.BackTop/>
+          <TableWorkHours
+              isUpdateTable={isUpdateTable}
+              // openDrawer={openDrawer}
+              // onDelete={handleDeleteUnit}
+          />
+          {/*<CreateModalUnit*/}
+          {/*  isOpen={isModalOpen}*/}
+          {/*  createItem={handleCreateUnit}*/}
+          {/*  onCancel={() => setIsModalOpen(false)}*/}
+          {/*/>*/}
+          {/*<UpdateDrawerUnit*/}
+          {/*  isOpen={isDrawerOpen}*/}
+          {/*  selectedItemId={selectedUnitId}*/}
+          {/*  updateItem={handleUpdateUnit}*/}
+          {/*  onCancel={() => setIsDrawerOpen(false)}*/}
+          {/*/>*/}
+        </div>
+    );
+  };
