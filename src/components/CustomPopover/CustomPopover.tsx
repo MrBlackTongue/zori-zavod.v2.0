e2fac4b69b28d7fac115558e34c8b185a0e4bee1
MrBlackTongue <Lana-14-92@mail.ru>
2023-10-25 10:37:46 +0400

    DEV-70: Add CustomPopover

---
+++ src/components/CustomPopover/CustomPopover.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode, useState } from 'react';
+import { Button, Popover } from 'antd';
+import { QuestionOutlined } from '@ant-design/icons';
+
+interface CustomPopoverProps {
+  content: ReactNode;
+}
+
+export const CustomPopover: React.FC<CustomPopoverProps> = ({ content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenChange = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
+
+  return (
+    <Popover
+      placement="bottomLeft"
+      trigger="click"
+      onOpenChange={handleOpenChange}
+      content={content}>
+      <Button
+        type="primary"
+        size="small"
+        shape="circle"
+        ghost={!isOpen}
+        style={{ marginRight: 7 }}>
+        <QuestionOutlined />
+      </Button>
+    </Popover>
+  );
+};
