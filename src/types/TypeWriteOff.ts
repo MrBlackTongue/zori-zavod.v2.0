c9164c4b5c9f709a040b2e68e9f68596ac7ca6ca
MrBlackTongue <Lana-14-92@mail.ru>
2024-01-26 20:30:05 +0400

    DEV-157: Change TypeWriteOff

--- src/types/TypeWriteOff.ts
+++ src/types/TypeWriteOff.ts
@@ -1,6 +1,5 @@
 import { TypeEmployee } from './TypeEmployee';
 import { TypeProductionType } from './TypeProductionType';
-import { FormInstance } from 'antd/lib/form';
 
 export type TypeWriteOff = {
   id?: number;
@@ -8,22 +7,3 @@ export type TypeWriteOff = {
   productionType?: TypeProductionType;
   description?: string;
 };
-
-export type TypeWriteOffFormValue = {
-  id?: number;
-  employee?: number;
-  productionType?: number;
-  description?: string;
-};
-
-export interface FormWriteOffProps {
-  form: FormInstance;
-  allEmployee: TypeEmployee[];
-  onChangeEmployee: (value: string) => void;
-  onClearEmployee: () => void;
-  onSearchEmployee: (input: string, option: any) => boolean;
-  allProductionType: TypeProductionType[];
-  onChangeProductionType: (value: string) => void;
-  onClearProductionType: () => void;
-  onSearchProductionType: (input: string, option: any) => boolean;
-}
