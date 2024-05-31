7035579926f7a9ec918f40c6c9f865a2417cdff3
MrBlackTongue <Lana-14-92@mail.ru>
2024-05-31 11:09:58 +0700

    DEV-235: Add react-draggable

--- src/components/atoms/FormModal/FormModal.tsx
+++ src/components/atoms/FormModal/FormModal.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Form, Modal } from 'antd';
 import { ModalProps } from 'antd/lib/modal';
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 
 interface FormModalProps<T> extends ModalProps {
   isOpen: boolean;
@@ -17,6 +18,14 @@ export const FormModal = <T extends object>({
   ...modalProps
 }: FormModalProps<T>) => {
   const [form] = Form.useForm();
+  const [disabled, setDisabled] = useState(true);
+  const [bounds, setBounds] = useState({
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+  });
+  const draggleRef = useRef<HTMLDivElement>(null);
 
   const handleSubmit = (): void => {
     form
@@ -35,14 +44,52 @@ export const FormModal = <T extends object>({
     onCancel();
   };
 
+  const onStart = (_event: DraggableEvent, uiData: DraggableData) => {
+    const { clientWidth, clientHeight } = window.document.documentElement;
+    const targetRect = draggleRef.current?.getBoundingClientRect();
+    if (!targetRect) {
+      return;
+    }
+    setBounds({
+      left: -targetRect.left + uiData.x,
+      right: clientWidth - (targetRect.right - uiData.x),
+      top: -targetRect.top + uiData.y,
+      bottom: clientHeight - (targetRect.bottom - uiData.y),
+    });
+  };
+
   return (
     <Modal
       {...modalProps}
-      open={isOpen}
+      title={
+        <div
+          style={{ width: '100%', cursor: 'move' }}
+          onMouseOver={() => {
+            if (disabled) {
+              setDisabled(false);
+            }
+          }}
+          onMouseOut={() => {
+            setDisabled(true);
+          }}
+          onFocus={() => {}}
+          onBlur={() => {}}>
+          {modalProps.title}
+        </div>
+      }
       okText={'Сохранить'}
       cancelText={'Отмена'}
+      open={isOpen}
       onOk={handleSubmit}
-      onCancel={handleReset}>
+      onCancel={handleReset}
+      modalRender={modal => (
+        <Draggable
+          disabled={disabled}
+          bounds={bounds}
+          onStart={(event, uiData) => onStart(event, uiData)}>
+          <div ref={draggleRef}>{modal}</div>
+        </Draggable>
+      )}>
       {renderForm(form)}
     </Modal>
   );
