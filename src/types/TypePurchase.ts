460dd98f844bd6b7c7c3e3b4023625abc2b33ef3
MrBlackTongue <Lana-14-92@mail.ru>
2024-08-18 09:59:54 +0300

    Main: Hotfix TypePurchase

--- src/types/TypePurchase.ts
+++ src/types/TypePurchase.ts
@@ -1,11 +1,11 @@
-import { TypeProduct } from './TypeProduct';
 import { Dayjs } from 'dayjs';
+import { TypeItem } from './TypeItem';
 
 export type TypePurchase = {
   id?: number;
   amount?: number;
   cost?: number;
   date?: Dayjs | string;
-  product?: TypeProduct;
+  item?: TypeItem;
   paid?: boolean;
 };
